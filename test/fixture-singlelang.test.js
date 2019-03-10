2469021d17d01f7726cd985d4d6e8132ed487474
Reto Kaiser <reto@retoonline.com>
2019-03-10 18:19:37 +0100

    Add test with single language only

---
+++ test/fixture-singlelang.test.js
@@ -0,0 +1,31 @@
+const Fixture = require('./Fixture')
+
+describe('Fixture "singlelang"', () => {
+  let fixture = new Fixture('nuxt.config-singlelang', 5062)
+
+  beforeAll(async () => {
+    await fixture.setUp()
+  })
+
+  afterAll(async () => {
+    await fixture.tearDown()
+  })
+
+  describe('/en/foo', () => {
+    fixture.testUrl('/en/foo', html => {
+      expect(html).toContain('Foo page')
+      expect(html).toContain('Hello World!')
+      expect(html).toMatch(/<a href="\/en\/foo".*?>foo<\/a>/)
+      expect(html).toMatch(/<a href="\/en\/bar".*?>bar<\/a>/)
+      expect(html).toMatch(new RegExp('<link [^>]*href="/en/foo" [^>]*rel="alternate" [^>]*hreflang="en"[^>]*>'))
+      expect(html).toMatch(new RegExp('<link [^>]*href="/foo" [^>]*rel="alternate" [^>]*hreflang="x-default"[^>]*>'))
+    })
+  })
+
+  describe('/foo', () => {
+    fixture.testUrl('/foo', html => {
+      expect(html).toContain('Foo page')
+      expect(html).toContain('Hello World!')
+    })
+  })
+})
